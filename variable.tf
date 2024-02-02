894322770fd1d9cf3a9b9e94f9aeb361b2664539
Shilpa Joshi <148430632+Shilpa20010@users.noreply.github.com>
2024-02-02 19:54:37 +0530

    Add files via upload
    Changes done

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "CLIENT_ID" {
+  type = string
+}
+
+variable "CLIENT_SECRET" {
+  type = string
+}
+
+variable "ACCOUNT_ID" {}
+
+variable "region" {
+  default = "ap-south-1"
+}
+variable "databricks_account_username" {}
+variable "databricks_account_password" {}
